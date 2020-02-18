aab063b911aaea968af8ffb3e9c5894afe8a3587
Adriano Veríssimo <adrverissimo@yahoo.com.br>
2020-02-17 22:44:04 -0300

    Getting the obstacles to show after X seconds, and changing the level of the stage after X seconds.

--- Assets/Scripts/LevelSpawner.cs
+++ Assets/Scripts/LevelSpawner.cs
@@ -1,14 +1,14 @@
 ﻿using UnityEngine;
-using System;
 using System.Collections;
 using System.Collections.Generic;
 
 public class LevelSpawner : MonoBehaviour
 {
-    public Level[] levelConfig;
+    public LevelConfig[] levelConfig;
 
     private int currentLevel = 0;
     private Coroutine coroutineCurrentLevel;
+    private Coroutine coroutineSpawnObject;
 
     // Use this for initialization
     void Start()
@@ -23,7 +23,7 @@ public class LevelSpawner : MonoBehaviour
     }
 
     
-    public Level GetCurrentLevel()
+    public LevelConfig GetCurrentLevel()
     {
         return levelConfig[currentLevel];
     }
@@ -45,24 +45,54 @@ public class LevelSpawner : MonoBehaviour
 
         SetCurrentLevel(currentLevel - 1);
     }
+    public void FinishLevel()
+    {
+        StopCoroutine(coroutineSpawnObject);
+    }
 
     public IEnumerator LevelTimer()
     {
         while (true)
         {
             print("starting level: " + currentLevel);
+
+            coroutineSpawnObject = StartCoroutine(GetCurrentLevel().SpawnObject());
+
             yield return new WaitForSeconds(GetCurrentLevel().levelDuration);
+
             print("finishing level: " + currentLevel);
 
+            FinishLevel();
             IncreaseLevel();
         }
     }
 }
 
-[Serializable]
-public class Level
+[System.Serializable]
+public class LevelConfig
 {
-    public GameObject[] objectsToSpawn;
+    public LevelObject[] listObjects;
     public float levelDuration = 10f;
     public float instantiationInterval = 1f;
+
+    public IEnumerator SpawnObject()
+    {
+        while (true)
+        {
+            int rand = Random.Range(0, listObjects.Length);
+            LevelObject objSpawn = listObjects[rand];
+            GameObject obj = objSpawn.objectToSpawn;
+
+            MonoBehaviour.Instantiate(obj, objSpawn.transformPosition.position, Quaternion.identity);
+
+            yield return new WaitForSeconds(instantiationInterval);
+        }
+    }
+}
+
+[System.Serializable]
+public class LevelObject
+{
+    public Transform transformPosition;
+    public GameObject objectToSpawn;
 }
---
+++ Assets/Scripts/Obstacle.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Obstacle : MonoBehaviour
+{
+    public Rigidbody2D rb2D;
+    public Vector2 velocity;
+
+    private void Start()
+    {
+        rb2D.velocity = velocity;
+    }
+
+    private void Update()
+    {
+        
+    }
+}
