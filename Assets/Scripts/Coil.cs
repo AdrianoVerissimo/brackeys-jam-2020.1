d74567cd3262c4f2fb352278a396ac6c493b5624
Adriano Veríssimo <adrverissimo@yahoo.com.br>
2020-02-21 17:29:55 -0300

    Making the coil script inherit from the obstacle script.

--- Assets/Scripts/Coil.cs
+++ Assets/Scripts/Coil.cs
@@ -2,53 +2,24 @@
 using System.Collections;
 using DG.Tweening;
 
-public class Coil : MonoBehaviour
+public class Coil : Obstacle
 {
     public Transform destinationMoveY;
     public float moveDuration = 1f;
-    public string[] playerTags;
     public bool resetGravityAfterMove = true;
 
-    // Use this for initialization
-    void Start()
+    public override void CollisionAction()
     {
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
-    }
-
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-        if (CheckPlayerTag(collision.tag))
+        Tweener tween = player.transform.DOMoveY(destinationMoveY.position.y, moveDuration);
+        if (resetGravityAfterMove)
         {
-            Tweener tween = collision.transform.DOMoveY(destinationMoveY.position.y, moveDuration);
-            if (resetGravityAfterMove)
-            {
-                tween.OnComplete(
-                    () => {
-                        Vector2 newVel = collision.GetComponent<Rigidbody2D>().velocity;
-                        newVel.y = 0f;
-                        collision.GetComponent<Rigidbody2D>().velocity = newVel;
-                        }
-                    );
-            }
+            tween.OnComplete(
+                () => {
+                    Vector2 newVel = player.GetComponent<Rigidbody2D>().velocity;
+                    newVel.y = 0f;
+                    player.GetComponent<Rigidbody2D>().velocity = newVel;
+                }
+                );
         }
-    }
-
-    public bool CheckPlayerTag(string value)
-    {
-        foreach (var item in playerTags)
-        {
-            if (value == item)
-            {
-                return true;
-            }
-        }
-
-        return false;
     }
 }
