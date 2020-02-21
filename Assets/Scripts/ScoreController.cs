3d938185a1f180b44c8e0340277b9fdc3f3b9e36
Adriano Veríssimo <adrverissimo@yahoo.com.br>
2020-02-21 18:53:09 -0300

    Creating a score tick to constantly keep adding score.

---
+++ Assets/Scripts/ScoreController.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScoreController : MonoBehaviour
+{
+    public static ScoreController Instance;
+
+    public int scoreGainValue = 10;
+    public float secondsToGainScore = 1f;
+
+    protected int currentScore = 0;
+    protected int highscore = 0;
+
+    // Use this for initialization
+    void Start()
+    {
+        Instance = this;
+
+        StartCoroutine(ScoreTick());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public IEnumerator ScoreTick()
+    {
+        yield return new WaitForSeconds(secondsToGainScore);
+
+        EarnScore(scoreGainValue);
+        StartCoroutine(ScoreTick());
+    }
+
+    public virtual void EarnScore(int value)
+    {
+        currentScore += value;
+
+        if (currentScore < 0)
+            currentScore = 0;
+    }
+}
