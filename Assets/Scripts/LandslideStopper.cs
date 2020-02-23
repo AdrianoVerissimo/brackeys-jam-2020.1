7b038e5413bae03a9f3e30f86319320a52e65c42
Adriano Veríssimo <adrverissimo@yahoo.com.br>
2020-02-23 01:18:10 -0300

    Fixing the issue that, when dealing with various coil obstacles, the landslide was inevitable.

---
+++ Assets/Scripts/LandslideStopper.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LandslideStopper : MonoBehaviour
+{
+    public string[] playerTags;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (CheckPlayerTag(collision.tag))
+        {
+            LandslidingMovement.Instance.Pause();
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (CheckPlayerTag(collision.tag))
+        {
+            LandslidingMovement.Instance.Unpause();
+        }
+    }
+
+    public bool CheckPlayerTag(string value)
+    {
+        foreach (var item in playerTags)
+        {
+            if (value == item)
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+}
--- Assets/Scripts/LandslidingMovement.cs
+++ Assets/Scripts/LandslidingMovement.cs
@@ -4,17 +4,36 @@ using UnityEngine;
 
 public class LandslidingMovement : MonoBehaviour
 {
+    public static LandslidingMovement Instance;
+    public Rigidbody2D rb2d;
+
     public float defaultSpeed = 2f;
     [HideInInspector] public float speed;
 
+    protected RigidbodyType2D originalBodyType;
+
     private void Start()
     {
+        Instance = this;
+        originalBodyType = rb2d.bodyType;
+
         speed = defaultSpeed;
     }
 
     private void FixedUpdate()
     {
-        transform.Translate(Vector3.right * speed * Time.deltaTime);
+        //transform.Translate(Vector3.right * speed * Time.deltaTime);
+        rb2d.velocity = Vector3.right * speed * Time.deltaTime;
+    }
+
+    public void Pause()
+    {
+        print("oi");
+        rb2d.bodyType = RigidbodyType2D.Static;
+    }
+    public void Unpause()
+    {
+        rb2d.bodyType = originalBodyType;
     }
 
 }
