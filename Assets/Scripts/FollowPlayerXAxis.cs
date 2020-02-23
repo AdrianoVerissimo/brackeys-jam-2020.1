f809d2ba4a60b835079dc93a81278f9c14f6d308
Alan Pereira <alan.registro@gmail.com>
2020-02-22 23:09:13 -0300

    - Added a Landsliding VFX in the left bottom of the screen

---
+++ Assets/Scripts/FollowPlayerXAxis.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayerXAxis : MonoBehaviour
+{
+
+    private RunnerCharacterController2D player;
+    public float distanceX = 0;
+
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").GetComponent<RunnerCharacterController2D>();
+    }
+
+    void Update()
+    {
+        transform.position = new Vector3(player.transform.position.x - distanceX, transform.position.y, 1f);
+    }
+}
