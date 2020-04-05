578c2f2b69481e84ede2aeb767fb80cc9d84b63f
Adriano Veríssimo <adrverissimo@yahoo.com.br>
2020-04-05 17:59:19 -0300

    Adding the action to exit the game when pressing Escape or Q button.

---
+++ Assets/Scripts/ExitGame.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ExitGame : MonoBehaviour
+{
+    public string exitButtonName = "Exit";
+
+    void Update()
+    {
+        if (Input.GetButtonDown(exitButtonName))
+        {
+            Exit();
+        }
+    }
+
+    public virtual void Exit()
+    {
+        Application.Quit();
+    }
+}
